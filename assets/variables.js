99d5b202d5c3f415aa4579ab71283a8fcb82af93
Fakhri <fahriakmal23@gmail.com>
2020-05-13 17:02:19 +0700

    ajeng 1

--- assets/variables.js
+++ assets/variables.js
@@ -1,10 +1,10 @@
 export default {
-  nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
+  nicknames: ["Ajengg", "Hun", "Huneyy", "Sayang", "Ma Gurllll"],
   greetings: {
     evening: "Good Evening",
-    afternoon: "Good Afternoon",
-    day: "Good Day",
-    morning: "Good Morning",
+    afternoon: "Selamat Berbuka",
+    day: "Great Day",
+    morning: "Pagi hunnn &#128420;",
     night: "Good Night"
   }
 };
